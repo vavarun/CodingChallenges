19b3d96eeb2ba2240e8933a29fa84c965c7fb9c1
Varun A <varunag@outlook.com>
2019-05-19 23:21:46 +0100

    Sum Of Pairs v1

---
+++ CodeWars/SumOfPairs-5kyu.js
@@ -0,0 +1,13 @@
+var sum_pairs = function(ints, s) {
+  if (ints.length < 2) return undefined;
+  let intSet = new Set();
+  intSet.add(ints[0]);
+  for (let i = 1; i < ints.length; ++i) {
+    let needed = s - ints[i];
+    if (intSet.has(needed)) {
+      return [needed, ints[i]];
+    }
+    intSet.add(ints[i]);
+  }
+  return undefined;
+};
